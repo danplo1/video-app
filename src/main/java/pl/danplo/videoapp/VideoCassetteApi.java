bd913bd0d44b2529b7c9fe37d4d31c993cf9b35f
danplo1 <budweiser401@wp.pl>
2019-02-28 21:36:46 +0100

    searching, adding, removing elements in postman

--- src/main/java/pl/danplo/videoapp/VideoCassetteApi.java
+++ src/main/java/pl/danplo/videoapp/VideoCassetteApi.java
@@ -1,9 +1,6 @@
 package pl.danplo.videoapp;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.time.LocalDate;
@@ -12,14 +9,13 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/cassettes")
+@RequestMapping("/api/cassetts")
 public class VideoCassetteApi {
 
     private List<VideoCassette> videoCassettes;
 
     public VideoCassetteApi() {
         videoCassettes = new ArrayList<>();
-
         videoCassettes.add(new VideoCassette(1L, "Chłopaki nie płaczą", LocalDate.of(2000, 6, 20)));
         videoCassettes.add(new VideoCassette(2L, "Bohemian Rhapsody", LocalDate.of(2018, 9, 10)));
         videoCassettes.add(new VideoCassette(3L, "Spectre", LocalDate.of(2015, 10, 23)));
@@ -32,12 +28,28 @@ public class VideoCassetteApi {
         return videoCassettes;
     }
 
-    @GetMapping("/all")
+    @GetMapping
     public VideoCassette getById(@RequestParam int index) {
         Optional<VideoCassette> first = videoCassettes.stream().
                 filter(element -> element.getId() == index).findFirst();
         return first.get();
         // jeżeli element ma takie samo ID jak to z indexu, to go pobieram
+    }
+
+    @PostMapping
+    public boolean addVideo(@RequestBody VideoCassette videoCassette) {
+        //metoda webowa
+        return videoCassettes.add(videoCassette);
+    }
+
+    @PutMapping //nadpisywanie elementów
+    public boolean updateVideo(@RequestBody VideoCassette videoCassette) {
+        return videoCassettes.add(videoCassette);
+    }
+
+    @DeleteMapping //usuwanie elementów
+    public boolean deleteVideo(@RequestParam int index) {
+        return videoCassettes.removeIf(element -> element.getId()== index);
     }
 
 }
